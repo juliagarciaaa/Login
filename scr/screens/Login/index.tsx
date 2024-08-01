1d81c8c68949dc3abbc7e95a6fb568b002b100c9
juliagarciaaa <juliasivagarcia3@gmail.com>
2024-08-01 10:38:57 -0300

    Mensagem

--- scr/screens/Login/index.tsx
+++ scr/screens/Login/index.tsx
@@ -6,6 +6,8 @@ import { styles } from "./styles"
 import { colors } from "../../styles/colors";
 import { ComponentButtonInterface } from '../../components';
 import { LoginTypes } from '../../navigations/login.navigation';
+import { useAuth } from '../../hook/auth';
+import { AxiosError } from 'axios';
 
 
 export interface IAuthenticate {
@@ -14,9 +16,18 @@ export interface IAuthenticate {
 }
 export function Login({ navigation }: LoginTypes){
     const [data, setData] = useState<IAuthenticate>();
+    const { signIn, setLoading } = useAuth()
     async function handSingIn(){
         if (data?.email && data.password){
-            console.log(data)
+            setLoading(true)
+            try{
+                await signIn(data)
+            }catch(error){
+                const err = error as AxiosError
+                const msg = err.response?.data as string
+                Alert.alert(msg)
+            }
+            setLoading(false)
         } else {
             Alert.alert("Preencha todos os campos!!!");
         }
--- scr/screens/Register/index.tsx
+++ scr/screens/Register/index.tsx
@@ -5,6 +5,9 @@ import { styles } from './styles';
 import { colors } from '../../styles/colors';
 import { LoginTypes } from '../../navigations/login.navigation';
 import { ComponentButtonInterface } from '../../components';
+import { apiUser } from '../../services/data';
+import { AxiosError } from 'axios';
+import { useAuth } from '../../hook/auth';
 
 export interface IRegister {
     name?: string
@@ -14,9 +17,20 @@ export interface IRegister {
 
 export function Register({ navigation }: LoginTypes) {
     const [data, setData] = useState<IRegister>();
+    const { setLoading } = useAuth ()
     async function handleRegister() {
         if (data?.email && data.name && data.password) {
-            console.log(data)
+            setLoading(true)
+            try {
+                const response = await apiUser.register(data)
+                Alert.alert(`${response.data.name} Cadastrado!`)
+                navigation.navigate("Login")
+            } catch (error) {
+                const err = error as AxiosError
+                const msg = err.response?.data as string
+                Alert.alert(msg)
+            }
+           setLoading(false)
         } else {
             Alert.alert("Preencha todos os campos!!!");
         }
